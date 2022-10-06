fd0d9a6bee217302d2251327cbe5d7d558ce58e9
Thomas <thomas.lefloch@eisti.fr>
2022-10-06 18:54:54 +0200

    fix: working frame reading

--- include/app.c
+++ include/app.c
@@ -1,19 +1,19 @@
 /**
-* \file main.c
-* \brief Description breve
-* \details Details ou notes d’implementation (peut rester vide)
-* \author Mathilde HAMON <<hamonmathi@cy-tech.fr>>
-*/
+ * \file main.c
+ * \brief Description breve
+ * \details Details ou notes d’implementation (peut rester vide)
+ * \author Mathilde HAMON <<hamonmathi@cy-tech.fr>>
+ */
 
 #include "drv_api.h"
 
 /**
-* \brief Description breve de la fonction.
-* \details Details ou notes d’implementation (peut rester vide)
-* \param param1 : Description
-* \param param2 : Description
-* \return int32_t : main 
-*/
+ * \brief Description breve de la fonction.
+ * \details Details ou notes d’implementation (peut rester vide)
+ * \param param1 : Description
+ * \param param2 : Description
+ * \return int32_t : main
+ */
 int32_t main(int argc, char const *argv[])
 {
     int32_t drvFd;
@@ -22,20 +22,14 @@ int32_t main(int argc, char const *argv[])
     // Open connection with driver
     drvFd = drv_open();
     printf("drvFd = %d\n", drvFd);
- 
-        while (1)
-        {
-
-            drv_read_udp_10ms(drvFd, udpFrame);
-            printf("%u\n", udpFrame[0]);
-            
-        }
-        
-        
+    drv_read_udp_10ms(drvFd, udpFrame);
+    for (int i = 0; i < DRV_UDP_10MS_FRAME_SIZE; i++)
+    {
+        printf("udpFrame[%d] = %d\n", i, udpFrame[i]);
+    }
     
     // Close connection with driver
     drvClose = drv_close(drvFd);
 
     return (drvClose);
 }
-
