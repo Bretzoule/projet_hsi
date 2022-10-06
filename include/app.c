d6e4b11d9f24806e6acddbef7c47dc54384aa9da
Mathilde Hamon <mathilde.hamon@eisti.fr>
2022-10-06 17:08:49 +0200

    feat: first steps reading udp frame

---
+++ include/app.c
@@ -0,0 +1,41 @@
+/**
+* \file main.c
+* \brief Description breve
+* \details Details ou notes d’implementation (peut rester vide)
+* \author Mathilde HAMON <<hamonmathi@cy-tech.fr>>
+*/
+
+#include "drv_api.h"
+
+/**
+* \brief Description breve de la fonction.
+* \details Details ou notes d’implementation (peut rester vide)
+* \param param1 : Description
+* \param param2 : Description
+* \return int32_t : main 
+*/
+int32_t main(int argc, char const *argv[])
+{
+    int32_t drvFd;
+    uint8_t udpFrame[DRV_UDP_10MS_FRAME_SIZE];
+    int32_t drvClose;
+    // Open connection with driver
+    drvFd = drv_open();
+    printf("drvFd = %d\n", drvFd);
+ 
+        while (1)
+        {
+
+            drv_read_udp_10ms(drvFd, udpFrame);
+            printf("%u\n", udpFrame[0]);
+            
+        }
+        
+        
+    
+    // Close connection with driver
+    drvClose = drv_close(drvFd);
+
+    return (drvClose);
+}
+
